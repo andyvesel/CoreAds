687556f9f507ad926ce558de33d528a7682405f8
Gabriela Dan <gabriela.dan@bioelite.ro>
2019-01-18 11:14:46 +0000

    navigation fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-    
+
     devise_for :users, :controllers => {:registrations => "registrations"}
+        
+    resources :sessions, :only => [:new, :create, :destroy]
     
-    
-     resources :sessions, :only => [:new, :create, :destroy]
-   devise_scope :user do
+    devise_scope :user do
         get 'login', to: 'devise/sessions#new'
     end
     
-   
-
+    devise_scope :user do
+        get 'signup', to: 'devise/registrations#new'
+    end
     
-      root to: 'pages#index'
+    
+    root to: 'pages#index'
 end
 
